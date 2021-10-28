2deb88eff006f13502a1013c3cd6d806a3a84f95
Sergey S <ss@vse-podklyuch.ru>
2021-10-28 17:04:28 +0300

    Install Jquery + fixed top menu was made

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+
 $(function() {
-    console.log("proverka");
+    let header = $("#header");
+    let intro = $("#intro");
+    let scrollPos = $(window).scrollTop();
+
+
+
+
+    $(window).on("scroll load resize", function () {
+        let introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        if( scrollPos >introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+        console.log(scrollPos);
+
+    });
+
+
+
 });
